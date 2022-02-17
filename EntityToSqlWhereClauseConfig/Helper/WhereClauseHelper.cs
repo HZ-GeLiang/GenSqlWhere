ff3d051a40a91c98afb2d21ac4c57806fd324cdd
GeLiang <gel@ccf.com.cn>
2022-02-17 15:21:07 +0800

    签入

--- EntityToSqlWhereClauseConfig/Helper/WhereClauseHelper.cs
+++ EntityToSqlWhereClauseConfig/Helper/WhereClauseHelper.cs
@@ -11,41 +11,63 @@ namespace EntityToSqlWhereClauseConfig.Helper
         /// <summary>
         /// sql参数名转成数字的
         /// </summary>
-        /// <param name="whereclause"></param>
+        /// <param name="whereClause"></param>
         /// <returns></returns>
-
-        public static string ParamNameToNumber(string whereclause)
+        public static string ParamNameToNumber(string whereClause)
         {
-            if (string.IsNullOrWhiteSpace(whereclause))
+            if (string.IsNullOrWhiteSpace(whereClause))
             {
                 return null;
             }
 
             string pattern = "@[a-zA-Z0-9_]*";
-            var matches = Regex.Matches(whereclause, pattern);
+            var matches = Regex.Matches(whereClause, pattern);
             //要倒序替换
             for (int i = matches.Count - 1; i >= 0; i--)
             {
-                whereclause = whereclause.Replace(matches[i].Value, "@" + i);
+                whereClause = whereClause.Replace(matches[i].Value, "@" + i);
             }
-            return whereclause;
+            return whereClause;
         }
 
-        //public static System.Data.SqlClient.SqlParameter[] ParamToSqlParameter(Dictionary<string, object> param)
-        //{
-        //    if (param == null)
-        //    {
-        //        return new System.Data.SqlClient.SqlParameter[0];
-        //    }
-        //    var cmdParms = new System.Data.SqlClient.SqlParameter[param.Keys.Count];
-        //    var i = 0;
-        //    foreach (var key in param.Keys)
-        //    {
-        //        cmdParms[i] = new System.Data.SqlClient.SqlParameter(key, param[key]);
-        //        i++;
-        //    }
-        //    return cmdParms;
-        //}
+        /// <summary>
+        /// sql参数合并到sql语句中
+        /// </summary>
+        /// <param name="whereClause"></param>
+        /// <param name="parameters"></param>
+        /// <returns></returns>
+        public static string MergeParametersIntoSql(string whereClause, Dictionary<string, object> parameters)
+        {
+            if (string.IsNullOrWhiteSpace(whereClause))
+            {
+                return null;
+            }
+
+            string pattern = "@[a-zA-Z0-9_]*";
+            var matches = Regex.Matches(whereClause, pattern);
+
+            var typeString = typeof(string);
+            //要倒序替换
+            for (int i = matches.Count - 1; i >= 0; i--)
+            {
+                var sqlParameterName = matches[i].Value;
+                var sqlParameterValue = parameters[matches[i].Value];
+                if (sqlParameterValue.GetType() == typeString)
+                {
+                    whereClause = whereClause.Replace(sqlParameterName, $"'{sqlParameterValue}'");
+                }
+                else
+                {
+                    whereClause = whereClause.Replace(sqlParameterName, $"{sqlParameterValue}");
+                }
+            }
+            return whereClause;
+        }
+
+        public static string MergeParametersIntoSql(ValueTuple<string, Dictionary<string, object>> sqlAndParam)
+            => MergeParametersIntoSql(sqlAndParam.Item1, sqlAndParam.Item2);
+
+
 
     }
 }
