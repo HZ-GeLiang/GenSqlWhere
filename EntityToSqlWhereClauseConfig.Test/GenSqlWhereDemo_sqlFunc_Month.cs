d6535efae53b764b5b5f72010dfcf67e475f58b2
GeLiang <childliang@hotmail.com>
2022-02-20 00:13:33 +0800

    添加MonthIn示例(第二个用法)

--- EntityToSqlWhereClauseConfig.Test/GenSqlWhereDemo_sqlFunc_Month.cs
+++ EntityToSqlWhereClauseConfig.Test/GenSqlWhereDemo_sqlFunc_Month.cs
@@ -44,15 +44,14 @@ namespace EntityToSqlWhereClauseConfig.Test
     public class Input_sqlFun_MonthIn2
     {
         [MonthIn]
-        public List<int> DataCreatedAt { get; set; } // 必须和 Entity 的属性名一致
+        public string DataCreatedAt { get; set; } //最后要翻译为 List<int> 
     }
 
     public class Input_sqlFun_MonthIn3
     {
         [MonthIn]
-        public string DataCreatedAt { get; set; } //最后要翻译为 List<int> 
+        public List<int> DataCreatedAt { get; set; } // 必须和 Entity 的属性名一致
     }
-
 
     #endregion
 
@@ -115,7 +114,7 @@ namespace EntityToSqlWhereClauseConfig.Test
 
 
         [TestMethod]
-        public void MonthIn_eq()
+        public void MonthIn_eq_1()
         {
             var searchModel = new Input_sqlFun_MonthIn1()
             {
@@ -132,6 +131,29 @@ namespace EntityToSqlWhereClauseConfig.Test
 
             Dictionary<string, object> expectedParameters = new Dictionary<string, object>();
             expectedParameters.Add("@MonthIn", "1"); //sql 的 in(变量), 这个变量只能写一个, 所以,这里是string
+            Assert.AreEqual("((Month(DataCreatedAt) In (@MonthIn)))", whereClause);
+
+            DictionaryAssert.AreEqual(expectedParameters, parameters);
+        }
+
+        [TestMethod]
+        public void MonthIn_eq_2()
+        {
+            var searchModel = new Input_sqlFun_MonthIn2()
+            {
+                DataCreatedAt = "1,2,3"
+            };
+            var whereLambda = searchModel.CrateWhereLambda((Model_sqlFun_Month p) => { });
+            whereLambda[SearchType.@in] = new List<string>
+            {
+                nameof(searchModel.DataCreatedAt),
+            };
+            var exp = whereLambda.ToExpression();
+
+            (string whereClause, Dictionary<string, object> parameters) = exp.ToWhereClause();
+
+            Dictionary<string, object> expectedParameters = new Dictionary<string, object>();
+            expectedParameters.Add("@MonthIn", "1,2,3"); //sql 的 in(变量), 这个变量只能写一个, 所以,这里是string
             Assert.AreEqual("((Month(DataCreatedAt) In (@MonthIn)))", whereClause);
 
             DictionaryAssert.AreEqual(expectedParameters, parameters);
