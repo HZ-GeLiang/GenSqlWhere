e640d976d56e249016bc8dcd9aaf9bdf96ab55e5
GeLiang <childliang@hotmail.com>
2022-02-24 23:51:35 +0800

    1.5.1

--- NugetPackageTest/Program.cs
+++ NugetPackageTest/Program.cs
@@ -1,7 +1,7 @@
-﻿using EntityToSqlWhereClauseConfig;
-using ExpressionToSqlWhereClause;
-using System;
+﻿using System;
 using System.Collections.Generic;
+using ExpressionToSqlWhereClause;
+using ExpressionToSqlWhereClause.EntityConfig;
 
 namespace NugetPackageTest
 {
