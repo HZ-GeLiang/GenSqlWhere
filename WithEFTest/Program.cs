d98a0ee2e494125cf9d7da71bc40ed68acf181b3
GeLiang <childliang@hotmail.com>
2023-02-14 23:39:48 +0800

    单元测试

---
+++ WithEFTest/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using ExpressionToSqlWhereClause;
+using WithEFTest.ExtensionMethod;
+using ExpressionToSqlWhereClause.Helper;
+using System.Runtime.CompilerServices;
+
+var v3 = new DateTime(2023, 1, 1);
+var v4 = Guid.Parse("DEA5B56C-D1B1-4513-83E7-B58B9D3EBB81");
+var expression = default(Expression<Func<ExpressionSqlTest, bool>>)
+    .WhereIf(true, a => a.Id == 1)
+    .WhereIf(true, a => a.Name == "abc")
+    //.WhereIf(true, a => a.CreateAt > new DateTime(2023, 1, 1))
+    //.WhereIf(true, a => a.Flag.Value == Guid.Parse("DEA5B56C-D1B1-4513-83E7-B58B9D3EBB81"))
+    .WhereIf(true, a => a.CreateAt > v3)
+    //.WhereIf(true, a => a.Flag.Value == v4)
+    .WhereIf(true, a => a.Flag == v4)
+    ;
+
+var where = WhereClauseHelper.ToFormattableString(expression.ToWhereClause());
+
+string sql = $@"select * from ExpressionSqlTest where {where.whereClause} ";
+
+var fmbsql = FormattableStringFactory.Create(sql, where.parameters);
+
+
+using var db = new TestContext();
+
+var list = db.ExpressionSqlTests.FromSqlInterpolated(fmbsql);
+foreach (var item in list)
+{
+    Console.WriteLine(item);
+}
---
+++ WithEFTest/TestContext.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+public partial class TestContext : DbContext
+{
+    public TestContext()
+    {
+    }
+
+    public TestContext(DbContextOptions<TestContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<ExpressionSqlTest> ExpressionSqlTests { get; set; } 
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            var connStr = "Data Source = 127.0.0.1 ; Initial Catalog = EFCorePractice; Integrated Security = True";
+            optionsBuilder.UseSqlServer(connStr);
+        }
+    }
+     
+}
+
+public record class ExpressionSqlTest
+{
+
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public DateTime? CreateAt { get; set; }
+    public Guid? Flag { get; set; }
+}
+
+
+/*
+ create table ExpressionSqlTest
+(
+    Id       int identity,
+    Name     nvarchar(50),
+    CreateAt datetime,
+    Flag     uniqueidentifier
+)
+go
+
+create unique index ExpressionSqlTest_Id_uindex
+    on ExpressionSqlTest (Id)
+go
+
+
+INSERT INTO EFCorePractice.dbo.ExpressionSqlTest (Name, CreateAt, Flag) VALUES (N'abc', N'2023-02-14 23:17:33.000', N'DEA5B56C-D1B1-4513-83E7-B58B9D3EBB81')
+
+ */
+
