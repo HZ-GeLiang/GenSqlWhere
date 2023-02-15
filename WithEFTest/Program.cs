92a7df0b64f6bdb5a8392b12fe7c0fe9560e95fd
GeLiang <childliang@hotmail.com>
2023-02-15 22:11:36 +0800

    调整单元测试

--- WithEFTest/Program.cs
+++ WithEFTest/Program.cs
@@ -15,16 +15,16 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 //var s1_res = "SELECT * FROM table1 WHERE col1 = 'value1' AND (col2 = 'value2' OR col3 = 'value3')";
 //Console.WriteLine(s1_res);
 
-var v3 = new DateTime(2023, 1, 1);
-var v4 = Guid.Parse("DEA5B56C-D1B1-4513-83E7-B58B9D3EBB81");
+//var v3 = new DateTime(2023, 1, 1);
+//var v4 = Guid.Parse("DEA5B56C-D1B1-4513-83E7-B58B9D3EBB81");
 var expression = default(Expression<Func<ExpressionSqlTest, bool>>)
     .WhereIf(true, a => a.Id == 1)
     .WhereIf(true, a => a.Name == "abc")
-    //.WhereIf(true, a => a.CreateAt > new DateTime(2023, 1, 1))
-    //.WhereIf(true, a => a.Flag.Value == Guid.Parse("DEA5B56C-D1B1-4513-83E7-B58B9D3EBB81"))
-    .WhereIf(true, a => a.CreateAt > v3)
-    .WhereIf(true, a => a.Flag.Value == v4)
-    //.WhereIf(true, a => a.Flag == v4)
+    .WhereIf(true, a => a.CreateAt > new DateTime(2023, 1, 1))
+    .WhereIf(true, a => a.Flag.Value == Guid.Parse("DEA5B56C-D1B1-4513-83E7-B58B9D3EBB81"))
+    //.WhereIf(true, a => a.CreateAt > v3)
+    //.WhereIf(true, a => a.Flag.Value == v4)
+    ////.WhereIf(true, a => a.Flag == v4)
     ;
 
 var where = WhereClauseHelper.ToFormattableString(expression.ToWhereClause());
