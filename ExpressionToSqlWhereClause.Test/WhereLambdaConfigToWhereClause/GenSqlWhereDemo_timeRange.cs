5d41d3b26ffa2218b9b34be37c58866d22b9e535
GeLiang <childliang@hotmail.com>
2023-03-15 20:57:01 +0800

    增加单元测试的示例

--- ExpressionToSqlWhereClause.Test/WhereLambdaConfigToWhereClause/GenSqlWhereDemo_timeRange.cs
+++ ExpressionToSqlWhereClause.Test/WhereLambdaConfigToWhereClause/GenSqlWhereDemo_timeRange.cs
@@ -36,7 +36,7 @@ namespace ExpressionToSqlWhereClause.Test.WhereLambdaConfigToWhereClause
             var searchCondition = expression.ToWhereClause();
 
             Assert.AreEqual(searchCondition.WhereClause, "DataCreatedAt >= @DataCreatedAt And DataCreatedAt < @DataCreatedAt1");
-            var dict = new Dictionary<string, object>
+            Dictionary<string, object> dict = new ()
             {
                 { "@DataCreatedAt", searchModel.DataCreatedAtStart},
                 { "@DataCreatedAt1", DateTime.Parse("2021-8-9")},
@@ -44,6 +44,84 @@ namespace ExpressionToSqlWhereClause.Test.WhereLambdaConfigToWhereClause
 
             CollectionAssert.AreEqual(searchCondition.Parameters, dict);
         }
+
+
+        [TestMethod]
+        public void Test_timeRange_start_eq_end()
+        {
+            //DataCreatedAtStart == DataCreatedAtEnd  ,会按 2021.8.8 一整天的时间号来解析
+
+            var searchModel = new Input_timeRange_Attr()
+            {
+                DataCreatedAtStart = DateTime.Parse("2021-8-8"),
+                DataCreatedAtEnd = DateTime.Parse("2021-8-8"),
+            };
+            var whereLambda = new WhereLambda<model_timeRange, Input_timeRange_Attr>();
+            whereLambda.Search = searchModel;
+
+            whereLambda[SearchType.TimeRange] = new List<string>
+            {
+                nameof(searchModel.DataCreatedAtStart),
+                nameof(searchModel.DataCreatedAtEnd),
+                nameof(searchModel.DataUpdatedAtStart),
+                nameof(searchModel.DataUpdatedAtEnd),
+            };
+
+            var expression = whereLambda.ToExpression();
+            var searchCondition = expression.ToWhereClause();
+
+            Assert.AreEqual(searchCondition.WhereClause, "DataCreatedAt >= @DataCreatedAt And DataCreatedAt < @DataCreatedAt1");
+            Dictionary<string, object> dict = new ()
+            {
+                { "@DataCreatedAt", searchModel.DataCreatedAtStart},
+                { "@DataCreatedAt1", DateTime.Parse("2021-8-9")},
+            };
+
+            CollectionAssert.AreEqual(searchCondition.Parameters, dict);
+        }
+
+
+        [TestMethod]
+        public void Test_timeRange_start_eq_end_WhereIf()
+        {
+            //DataCreatedAtStart == DataCreatedAtEnd  ,会按 2021.8.8 一整天的时间号来解析
+
+            var searchModel = new Input_timeRange_Attr()
+            {
+                DataCreatedAtStart = DateTime.Parse("2021-8-8"),
+                DataCreatedAtEnd = DateTime.Parse("2021-8-8"),
+            };
+            var whereLambda = new WhereLambda<model_timeRange, Input_timeRange_Attr>();
+            whereLambda.Search = searchModel;
+
+            whereLambda[SearchType.TimeRange] = new List<string>
+            {
+                nameof(searchModel.DataCreatedAtStart),
+                nameof(searchModel.DataCreatedAtEnd),
+                nameof(searchModel.DataUpdatedAtStart),
+                nameof(searchModel.DataUpdatedAtEnd),
+            };
+
+            whereLambda.WhereIf[nameof(searchModel.DataCreatedAtStart)] =
+                    a => a.DataCreatedAtStart > new DateTime(2022, 8, 9);//  
+
+            //  因为上面的条件没生效, 所以翻译引擎会当  只有 DataCreatedAtEnd 有值来处理.
+            //所以, 这里要把  DataCreatedAtEnd 当没有值来处理
+            whereLambda.WhereIf[nameof(searchModel.DataCreatedAtEnd)] =
+                    a => a.DataCreatedAtStart > new DateTime(2022, 8, 9);
+
+            var expression = whereLambda.ToExpression();
+            var searchCondition = expression.ToWhereClause();
+
+            Assert.AreEqual(searchCondition.WhereClause, "");
+            Dictionary<string, object> dict = new ()
+            {
+            };
+
+            CollectionAssert.AreEqual(searchCondition.Parameters, dict);
+        }
+
+
 
         [TestMethod]
         public void Test_timeRange_Get时间精度()
