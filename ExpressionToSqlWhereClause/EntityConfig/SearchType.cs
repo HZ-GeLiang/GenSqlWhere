6ee16597a9fcf8611468a18d6f763e4c7c0b9c1c
GeLiang <childliang@hotmail.com>
2023-03-25 20:36:20 +0800

    注释说明

--- ExpressionToSqlWhereClause/EntityConfig/SearchType.cs
+++ ExpressionToSqlWhereClause/EntityConfig/SearchType.cs
@@ -21,7 +21,7 @@
         Eq,
 
         /// <summary>
-        /// 实际翻译成 in 还是 Equal , 根据 split() 后的个数而定 (sqlFunc除外)
+        /// 实际翻译成 in 还是 Equal, 根据 split() 后的个数而定(sqlFunc除外)
         /// </summary>
         In,
 
