0876ab027a09c21815dad5951dcc780307b9b14b
GeLiang <childliang@hotmail.com>
2023-04-26 11:25:22 +0800

    增加一个扩展方法

---
+++ ExpressionToSqlWhereClause.SqlServer/ExtensionMehtods/SearchConditionExtension.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq.Expressions;
+using System;
+using ExpressionToSqlWhereClause.ExtensionMethod;
+using ExpressionToSqlWhereClause.Helper;
+using Microsoft.Data.SqlClient;
+
+namespace ExpressionToSqlWhereClause.SqlServer.ExtensionMehtods
+{
+    public static class SearchConditionExtension
+    {
+        public static SqlParameter[] GetSqlParameter(this SearchCondition SearchCondition)
+        {
+            SqlParameter[] pms = WhereClauseHelper.ConvertParameters(SearchCondition.Parameters, (key, val) =>
+            {
+                return new SqlParameter { ParameterName = key, Value = val };
+            });
+
+            return pms;
+        }
+    }
+}
