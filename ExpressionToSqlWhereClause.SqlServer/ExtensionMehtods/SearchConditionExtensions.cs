47ad4a528568d0b72aa5882bd258f6b0949767f8
GeLiang <childliang@hotmail.com>
2023-05-15 08:52:04 +0800

    注释

--- ExpressionToSqlWhereClause.SqlServer/ExtensionMehtods/SearchConditionExtensions.cs
+++ ExpressionToSqlWhereClause.SqlServer/ExtensionMehtods/SearchConditionExtensions.cs
@@ -6,8 +6,16 @@ using Microsoft.Data.SqlClient;
 
 namespace ExpressionToSqlWhereClause.SqlServer.ExtensionMehtods
 {
-    public static class SearchConditionExtension
+    /// <summary>
+    /// 扩展
+    /// </summary>
+    public static class SearchConditionExtensions
     {
+        /// <summary>
+        /// 获得sql参数
+        /// </summary>
+        /// <param name="SearchCondition"></param>
+        /// <returns></returns>
         public static SqlParameter[] GetSqlParameter(this SearchCondition SearchCondition)
         {
             SqlParameter[] pms = WhereClauseHelper.ConvertParameters(SearchCondition.Parameters, (key, val) =>
