f44c84bcf25cb0b6c2cc471c80853638fde83b32
GeLiang <childliang@hotmail.com>
2023-11-10 17:12:44 +0800

    单元测试

--- ExpressionToSqlWhereClause.Test/EntityConfigToWhereClause/ExpressionDemo_值测试.cs
+++ ExpressionToSqlWhereClause.Test/EntityConfigToWhereClause/ExpressionDemo_值测试.cs
@@ -3,6 +3,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
+using System.Numerics;
 
 namespace ExpressionToSqlWhereClause.Test.EntityConfigToWhereClause
 {
@@ -13,6 +14,13 @@ namespace ExpressionToSqlWhereClause.Test.EntityConfigToWhereClause
         [TestMethod]
         public void eq_string为null值()
         {
+            Assert.ThrowsException<Exception>(() =>
+            {
+                // 提示 
+                Expression<Func<Student, bool>> expTest = o => string.IsNullOrEmpty(o.Url);
+                var aa = expTest.ToWhereClause().WhereClause;
+            }, "Please use(o.Url ?? \"\") != \"\" replace string.IsNullOrEmpty(o.Url).");
+
             Expression<Func<Student, bool>> expOr = a => a.Url == null;
             var searchCondition = expOr.ToWhereClause();
             Assert.AreEqual(searchCondition.WhereClause, "Url Is Null");
