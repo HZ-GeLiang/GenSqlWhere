ca91d671b3f3055844bc8506add978492c869bdb
GeLiang <childliang@hotmail.com>
2024-02-01 19:54:29 +0800

    增加单元测试

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using ExpressionToSqlWhereClause.EntityConfig;
+using System.Linq.Expressions;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Expression<Func<peo, bool>> exp = a => (a.Name ?? "") != "";
+            var exp2 = WhereLambdaHelper.GetExpression_HasValue<peo>("Name"); 
+ 
+            Console.WriteLine(exp.ToString() == exp2.ToString());
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+    class peo
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ ExpressionToSqlWhereClause.Test/WhereLambdaHelperTest.cs
@@ -0,0 +1,27 @@
+﻿using ExpressionToSqlWhereClause.EntityConfig;
+using ExpressionToSqlWhereClause.ExtensionMethod;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace ExpressionToSqlWhereClause.Test.LambdaToWhereClause
+{
+    class peo
+    {
+        public string Name { get; set; }
+    }
+
+    [TestClass]
+    public class WhereLambdaHelperTest
+    {
+        [TestMethod]
+        public void GetExpression_HasValue()
+        {
+            Expression<Func<peo, bool>> exp = a => (a.Name ?? "") != "";
+            var exp2 = WhereLambdaHelper.GetExpression_HasValue<peo>("Name");
+
+            Assert.AreEqual(true, exp.ToString() == exp2.ToString());
+        }
+    }
+}
