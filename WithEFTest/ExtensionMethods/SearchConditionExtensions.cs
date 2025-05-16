f522aa78ef8fcd88be1dd4a435f71663fc49a41b
GeLiang <childliang@hotmail.com>
2025-05-16 09:19:29 +0800

    签入

---
+++ WithEFTest/ExtensionMethods/SearchConditionExtensions.cs
@@ -0,0 +1,26 @@
+﻿using ExpressionToSqlWhereClause;
+using Microsoft.Data.SqlClient;
+
+namespace WithEFTest.ExtensionMethods
+{
+    /// <summary>
+    /// 扩展
+    /// </summary>
+    public static class SearchConditionExtensions
+    {
+        /// <summary>
+        /// 获得sql参数
+        /// </summary>
+        /// <param name="SearchCondition"></param>
+        /// <returns></returns>
+        public static SqlParameter[] GetSqlParameter(this SearchCondition SearchCondition)
+        {
+            SqlParameter[] pms = WhereClauseHelper.ConvertParameters(SearchCondition.Parameters, (key, val) =>
+            {
+                return new SqlParameter { ParameterName = key, Value = val };
+            });
+
+            return pms;
+        }
+    }
+}
--- WithEFTest/Program.cs
+++ WithEFTest/Program.cs
@@ -1,8 +1,14 @@
 ﻿using ExpressionToSqlWhereClause;
 using ExpressionToSqlWhereClause.ExtensionMethods;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
+using System.Data;
+using System.Data.Common;
 using System.Linq.Expressions;
 using WithEFTest.ExtensionMethods;
+
+using var _db = new TestContext();
+
 
 var expression = default(Expression<Func<ExpressionSqlTest, bool>>)
     .WhereIf(true, a => a.Id == 1)
@@ -10,18 +16,37 @@ var expression = default(Expression<Func<ExpressionSqlTest, bool>>)
     .WhereIf(true, a => a.CreateAt > new DateTime(2023, 1, 1))
     .WhereIf(true, a => a.Flag.Value == Guid.Parse("DEA5B56C-D1B1-4513-83E7-B58B9D3EBB81"));
 
-SearchCondition searchCondition = expression.ToWhereClause();
-
-var whereClause = WhereClauseHelper.GetFormattableStringClause(searchCondition);
-
-string sql = $@"select * from ExpressionSqlTest where {whereClause} ";
-
-FormattableString? formattableStr = WhereClauseHelper.CreateFormattableString(sql, searchCondition.Parameters.Values.ToArray());
-
-using var db = new TestContext();
-
-var list = db.ExpressionSqlTests.FromSqlInterpolated(formattableStr);
-foreach (var item in list)
 {
-    Console.WriteLine(item);
+    SearchCondition searchCondition = expression.ToWhereClause();
+    string? clause_FormattableString = WhereClauseHelper.GetFormattableStringClause(searchCondition);
+    string sql_str = $@"select * from ExpressionSqlTest where {clause_FormattableString}";
+    var pms = searchCondition.Parameters.Values.ToArray();
+    FormattableString? sql = WhereClauseHelper.CreateFormattableString(sql_str, pms);
+    var list = _db.ExpressionSqlTests.FromSqlInterpolated(sql);
+    foreach (var item in list)
+    {
+        Console.WriteLine(item);
+    }
+}
+
+
+{
+    //分页
+    SearchCondition searchCondition = expression.ToWhereClause();
+    var clause = searchCondition.WhereClause;
+    var param = searchCondition.Parameters;
+    var sql_count_str = $"select count(*) from V_CRM_Contacts where {clause}";
+
+    //var total = _db.ExecSqlReader<int>(sql_count_str, searchCondition.GetSqlParameter()).First();
+
+    //var pageIndex = dto.MaxResultCount;
+    //var pageSize = dto.SkipCount;
+
+    var pageIndex = 1;
+    var pageSize = 5;
+
+    var sql_query_str = $"select * from V_CRM_Contacts where {clause} OrderBy IsDeparture, ModifyDate desc " +
+        $@" OFFSET {(pageIndex - 1) * pageSize} ROWS FETCH NEXT {pageSize} ROWS ONLY";
+
+    //var list = _db.ExecSqlReader<V_CRM_Contacts>(sql_query_str, searchCondition.GetSqlParameter()).ToList();
 }
